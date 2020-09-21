526c6320f66e7b262a8c6280845667056b4c2218
Harsha Prajapati <harsha_1605@yahoo.com>
2020-09-21 19:08:07 +0530

    changes

---
+++ src/app/navbar/navbar.component 2.ts
@@ -0,0 +1,45 @@
+import { Component ,HostBinding, Output,EventEmitter  } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { slideInAnimation } from '../common/animation';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  animations:[
+    slideInAnimation
+    // animation triggers go here
+  ]
+})
+export class NavbarComponent {
+  @HostBinding('@.disabled')
+  public animationsDisabled = false;
+  title = 'angular-portfolio';
+  @Output() headerName = new EventEmitter<string>();
+ constructor(){
+
+ }
+ emitPage(name){
+  this.headerName.emit(name);
+ }
+ prepareRoute(outlet: RouterOutlet) {
+  return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+}
+ToggleNavBar() {
+  let element: HTMLElement = document.getElementsByClassName( 'navbar-toggler' )[ 0 ] as HTMLElement;
+  if ( element.getAttribute( 'aria-expanded' ) == 'true' ) {
+      element.click();
+  }
+}
+
+toggleAnimations() {
+  this.animationsDisabled = !this.animationsDisabled;
+}
+
+   scrollWin(x, y){  
+    window.scrollBy(x, y);
+  }
+
+}
+
+
